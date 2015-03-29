ee565fbf7dd58e0d8bf11fed272c6049bc29bcf3
Ahmad Issa <ahmadissa1994@gmail.com>
2015-03-29 20:56:10 +0200

    Core functionality finished.

--- src/Hexclock.java
+++ src/Hexclock.java
@@ -1,43 +1,71 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
 public class Hexclock implements Runnable {
 
-	// Laver en tråd af et Hexclock og kører det
+	private static JFrame frame = new JFrame("Hexclock");
+	private static JPanel panel = new JPanel(new BorderLayout());
+	private static JLabel label = new JLabel();
+	private Color rgbColor;
+	private static String rgbColorString;
+
 	public static void main(String[] args) {
 		Runnable r = new Hexclock();
 		Thread t = new Thread(r);
 		t.start();
+		makeGUI();
 	}
 
 	public void run() {
 		try {
-			// Kalder currentTime() hvert sekund
 			while (true) {
 				long millis = System.currentTimeMillis();
 				hashTime();
+				panel.setBackground(rgbColor);
+				label.setText(rgbColorString);
 				Thread.sleep(1000 - millis % 1000);
 			}
-			// Fanger InterrupetdException
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	// Printer tiden i formatet HH:mm:ss
-	public static void currentTime() {
+	public void currentTime() {
 		Calendar cal = Calendar.getInstance();
 		cal.getTime();
 		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 		System.out.println(sdf.format(cal.getTime()));
 	}
 
-	// Printer tiden i formatet #hhmmss
-	public static void hashTime() {
+	public void hashTime() {
 		Calendar cal = Calendar.getInstance();
 		cal.getTime();
 		SimpleDateFormat sdf = new SimpleDateFormat("#HHmmss");
 		System.out.println(sdf.format(cal.getTime()));
+		rgbColorString = sdf.format(cal.getTime());
+		rgbColor = Color.decode(rgbColorString);
+	}
+
+	public static void makeGUI() {
+		frame.setVisible(true);
+		frame.setSize(800, 600);
+		frame.add(panel);
+		panel.add(label, BorderLayout.CENTER);
+		label.setFont(new Font("Myriad Pro", Font.PLAIN, 60));
+		label.setForeground(Color.WHITE);
+		label.setText(rgbColorString);
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setVerticalAlignment(SwingConstants.CENTER);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
